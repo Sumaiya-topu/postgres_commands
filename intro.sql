8572231534d38df7cfdd37ca4211660424ca01d0
Sumaiya-topu <topusumaiya@gmail.com>
2023-10-21 18:51:21 +0600

    added queries : Alter table

--- intro.sql
+++ intro.sql
@@ -69,4 +69,62 @@ SELECT * FROM user1
 
 --Insert INTO table_name(col1, col2, ...)values(value1,value2,..),(value1,value2,...),(value1,value2,..);
 
---change CONSTRAINTS
+--## alter TABLE
+
+--1.change CONSTRAINTS, drop a column, change datatupeof a column, rename a column,set default values to a column, add constraint/delete constraint.
+
+ALTER Table user1 ADD COLUMN password varchar(255) NOT NULL;
+
+--adding new col name password
+
+-- delete table row without deleting a TABLE
+
+TRUNCATE TABLE user1;
+
+--drop a COLUMN from a TABLE
+
+ALTER Table user1 drop column age;
+
+-- add a COLUMN
+
+ALTER table user1 ADD COLUMN demo int;
+
+--changing the datatable
+
+ALTER Table user1 ALTER COLUMN demo type Float4;
+
+--setting a default value to an existing COLUMN
+
+ALTER Table user1 ALTER COLUMN demo set default 'Bangladesh';
+
+--remove default values to an existing COLUMN
+
+ALTER Table user1 ALTER COLUMN demo drop default;
+
+--rename a COLUMN
+
+ALTER Table user1 RENAME COLUMN demo to 'country';
+
+--this will rename the demon column to country
+
+-- adding a consraint
+
+ALTER table user1 ALTER COLUMN country set NOT NULL;
+
+--set the constraint into not NULL
+
+--drop a constrains,
+
+ALTER table user1 ALTER COLUMN country drop NOT NULL;
+
+--this will delete the constraint
+
+--add constraint except not null to an existing TABLE
+
+ALTER Table user1 ADD Constraint unique_email UNIQUE(email);
+
+--drop constraint except not null to an existing TABLE
+
+ALTER Table user1 DROP Constraint unique_email;
+
+SELECT * from user1 
